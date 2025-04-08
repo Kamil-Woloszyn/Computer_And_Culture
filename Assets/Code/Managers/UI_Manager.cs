1f1693f3a2e8234db8e3c0a7f9143a63dc9834e7
Kamil-Woloszyn <kamil.justkamil@gmail.com>
2025-04-08 09:58:25 +0100

    Repo New

---
+++ Assets/Code/Managers/UI_Manager.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class UI_Manager : MonoBehaviour
+{
+    [Serializable]
+    public class UI_OBJECTS
+    {
+        public GameObject gameObjectReferance;
+        public UI_Tabs key;
+
+    }
+    [SerializeField]
+    public UI_OBJECTS[] tabs = { };
+
+    private UI_Tabs currentTab = UI_Tabs.LOGIN_SCREEN;
+    private UI_Tabs previousTab = UI_Tabs.QUIT;
+    public static UI_Manager Singleton = null;
+    private void Awake()
+    {
+        if (Singleton != null && Singleton != this)
+        {
+            Destroy(Singleton);
+        }
+        else
+        {
+            Singleton = this;
+            DontDestroyOnLoad(Singleton);
+        }
+    }
+    private void Start()
+    {
+        SelectNewUI(currentTab);
+    }
+
+    public void ChangeUITab(UI_Tabs target)
+    {
+        if (currentTab == target) return;
+        if (!ValidateTabChange(target)) return;
+        if (target == UI_Tabs.QUIT) QuitGame();
+        //Code for changing the tabs from here on
+        DisableAllUI();
+        SelectNewUI(target);
+        previousTab = currentTab;
+        currentTab = target;
+    }
+    
+    
+
+    public void UI_GoBack()
+    {
+        DisableAllUI();
+        SelectNewUI(previousTab);
+
+        //Rotating the tabs around as we do not save any previous tabs (creating an infinite tab loop possible be careful)
+        UI_Tabs temp = previousTab;
+        previousTab = currentTab;
+        currentTab = temp;
+    }
+
+    private void DisableAllUI()
+    {
+        foreach (var tab in tabs)
+        {
+            //Disabling all UI objects
+             tab.gameObjectReferance.SetActive(false);
+        }
+    }
+
+    private void SelectNewUI(UI_Tabs _target)
+    {
+        foreach(var tab in tabs)
+        {
+            if (tab.key == _target)
+            {
+                //Enabling Correct Tab GameObject Reference
+                tab.gameObjectReferance.SetActive(true);
+            }
+        }
+    }
+
+    private bool ValidateTabChange(UI_Tabs _target)
+    {
+        bool isItOkToChangeTabs = true;
+
+        //RULES TO BE IMPLEMENTED
+
+        return isItOkToChangeTabs;
+    }
+
+    private void QuitGame()
+    {
+        //Close the application completely
+        Application.Quit();
+    }
+
+    public UI_Tabs GetCurrentTab()
+    {
+        return currentTab;
+    }
+
+    public UI_Tabs GetPreviousTab()
+    {
+        return currentTab;
+    }
+}
---
+++ Assets/Code/Test/UI_Test.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Test : MonoBehaviour
+{
+    public Button LOGIN_SCREEN;
+    public Button MAIN_MENU_STUDENT_SCREEN;
+    public Button MAIN_MENU_TEACHER_SCREEN;
+    public Button INSTRUCTIONS_SCREEN;
+    public Button STUDENT_GRADES_SCREEN;
+    public Button DIFFICULTY_SELECT_SCREEN;
+    public Button QUIT;
+
+    private void Start()
+    {
+        LOGIN_SCREEN.onClick.AddListener(GOTO_LOGINSCREEN);
+        MAIN_MENU_STUDENT_SCREEN.onClick.AddListener(GOTO_MAIN_MENU_STUDENT_SCREEN);
+        MAIN_MENU_TEACHER_SCREEN.onClick.AddListener(GOTO_MAIN_MENU_TEACHER_SCREEN);
+        INSTRUCTIONS_SCREEN.onClick.AddListener(GOTO_INSTRUCTIONS_SCREEN);
+        STUDENT_GRADES_SCREEN.onClick.AddListener(GOTO_STUDENT_GRADES_SCREEN);
+        DIFFICULTY_SELECT_SCREEN.onClick.AddListener(GOTO_DIFFICULTY_SELECT_SCREEN);
+        QUIT.onClick.AddListener(GOTO_QUIT);
+    }
+
+    private void GOTO_LOGINSCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.LOGIN_SCREEN);
+    }
+
+    private void GOTO_MAIN_MENU_STUDENT_SCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.MAIN_MENU_STUDENT_SCREEN);
+    }
+    private void GOTO_MAIN_MENU_TEACHER_SCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.MAIN_MENU_TEACHER_SCREEN);
+    }
+    private void GOTO_INSTRUCTIONS_SCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.INSTRUCTIONS_SCREEN);
+    }
+    private void GOTO_STUDENT_GRADES_SCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.STUDENT_GRADES_SCREEN);
+    }
+    private void GOTO_DIFFICULTY_SELECT_SCREEN()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.DIFFICULTY_SELECT_SCREEN);
+    }
+    private void GOTO_QUIT()
+    {
+        UI_Manager.Singleton.ChangeUITab(UI_Tabs.QUIT);
+    }
+
+}
