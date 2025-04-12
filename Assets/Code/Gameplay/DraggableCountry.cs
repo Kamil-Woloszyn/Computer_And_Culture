98f6fdf306e9941ad00a6e64b9aaae97f12148aa
SurlyMesser01 <leanscreen01@gmail.com>
2025-04-12 15:04:50 +0100

    Finished Gameplay/DragnDrop
    . Drag n Drop fully implemented
    . Gameplay logic finished

--- Assets/Code/Gameplay/DraggableCountry.cs
+++ Assets/Code/Gameplay/DraggableCountry.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 // Attached to the CountryCard prefab, stores reference to its data
 public class DraggableCountry : MonoBehaviour
 {
-    public CountryData data;  // Set at spawn time from GameManager
+    public CountryData data; // Is set at spawn time from GameManager
+
+
+
+
 }
---
+++ Assets/Code/Managers/SlotManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotManager : MonoBehaviour, IDropHandler
+{
+    private DraggableCountry cardInSlot;
+
+    public void AcceptCard(DraggableCountry card)
+    {
+        cardInSlot = card;
+        GameManager.Instance.ShowInputField(card);
+    }
+
+    public void ClearSlot()
+    {
+        cardInSlot = null;
+    }
+
+    public bool HasCard() => cardInSlot != null;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject droppedObj = eventData.pointerDrag;
+
+        if (droppedObj != null)
+        {
+            droppedObj.transform.SetParent(transform, false);
+            droppedObj.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
+            AcceptCard(droppedObj.GetComponent<DraggableCountry>());
+        }
+    }
+}
